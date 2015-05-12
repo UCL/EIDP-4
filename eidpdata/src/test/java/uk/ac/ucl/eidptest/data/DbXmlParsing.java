03b9354ff018ea8c9f40dc836936e221203bcd98
David Guzman <d.guzman@ucl.ac.uk>
2015-05-12 15:25:17 +0100

    Added unit tests for Stax parsing and JAXB

---
+++ eidpdata/src/test/java/uk/ac/ucl/eidptest/data/DbXmlParsing.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 David Guzman <d.guzman at ucl.ac.uk>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ucl.eidptest.data;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.stream.StreamSource;
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+import uk.ac.ucl.eidptest.data.jaxb.DatasetType;
+
+/**
+ *
+ * @author David Guzman <d.guzman at ucl.ac.uk>
+ */
+public class DbXmlParsing {
+    
+    private final String datasettag = "dataset";
+    private final String datasetid = "ROLES";
+
+    public DbXmlParsing() {
+    }
+
+    @Test
+    public void staxParse() throws XMLStreamException {  
+
+        XMLStreamReader msr = mock(XMLStreamReader.class);
+        when(msr.getLocalName()).thenReturn(datasettag);
+        when(msr.getAttributeValue(0)).thenReturn(datasetid);
+
+        XMLStreamReader xsr = generateXMLStreamReader();
+
+        assertEquals(msr.getLocalName(), xsr.getLocalName());
+        assertEquals(msr.getAttributeValue(0), xsr.getAttributeValue(0));
+
+        xsr.close();
+    }
+
+    @Test
+    public void fromStaxToJaxb() throws XMLStreamException, JAXBException {
+        XMLStreamReader xsr = generateXMLStreamReader();
+
+        JAXBContext jc = JAXBContext.newInstance(DatasetType.class);
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        JAXBElement<DatasetType> jb = unmarshaller.unmarshal(xsr, DatasetType.class);
+        DatasetType dataset = jb.getValue();
+
+        System.out.println(dataset.getTable().getName());
+        xsr.close();
+
+    }
+
+    private XMLStreamReader generateXMLStreamReader() throws XMLStreamException {
+        XMLInputFactory xif = XMLInputFactory.newFactory();
+        StreamSource xml = new StreamSource("src/test/resources/db.xml");
+        XMLStreamReader xsr = xif.createXMLStreamReader(xml);
+
+        while (xsr.hasNext()) {
+            int event = xsr.next();
+            if (XMLStreamConstants.START_ELEMENT == event
+                    && datasettag.equals(xsr.getLocalName())
+                    && datasetid.equals(xsr.getAttributeValue(0))) {
+                break;
+            }
+        }
+        return xsr;
+    }
+        
+}
