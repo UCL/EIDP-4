e52ea36148b2af775197f2d1643e73fd35f77917
David Guzman <d.guzman@ucl.ac.uk>
2016-02-16 17:23:14 +0000

    Added login controller and view

--- eidpng/app/scripts/controllers/login.js
+++ eidpng/app/scripts/controllers/login.js
@@ -7,8 +7,14 @@
  * # LoginCtrl
  * Controller of the eidpngApp
  */
-angular.module('eidpngApp')
-  .controller('LoginCtrl', function () {
+angular.module('login',['http-auth-interceptor'])
+  .controller('LoginCtrl', function ($scope, $http, authService) {
+    $scope.login = function(user) {
+      $http.post('http://localhost/api/auth/',user).success(function() {
+        authService.loginConfirmed();
+      });
+    };
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
