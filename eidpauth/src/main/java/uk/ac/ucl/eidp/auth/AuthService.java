9dc22351a410ea432c6e81689285bc63c08e02b5
David Guzman <d.guzman@ucl.ac.uk>
2016-09-15 12:28:01 +0100

    Re Sputnik CI build 910

--- eidpauth/src/main/java/uk/ac/ucl/eidp/auth/AuthService.java
+++ eidpauth/src/main/java/uk/ac/ucl/eidp/auth/AuthService.java
@@ -1,5 +1,6 @@
 package uk.ac.ucl.eidp.auth;
 
+import uk.ac.ucl.eidp.auth.model.RoleE;
 import uk.ac.ucl.eidp.auth.model.UserE;
 
 import java.util.Set;
@@ -7,7 +8,6 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import uk.ac.ucl.eidp.auth.model.RoleE;
 
 /**
  *
@@ -34,9 +34,8 @@ public class AuthService implements AuthServiceLocal {
   public boolean isAuthorised(String authId, String authToken, Set<String> rolesAllowed) {
     UserE user = userController.findByUsernameAndAuthToken(authId, authToken);
     if (user != null) {
-      // TODO : Check it might not work
       Set<String> userRoles = user.getRoles().stream().map(
-              (RoleE role) -> role.getRoleName()
+          (RoleE role) -> role.getRoleName()
       ).collect(Collectors.toSet());
       return rolesAllowed.containsAll(userRoles);
     } else {
