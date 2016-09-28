908acf197993002ae41c69c2faa77d2bf2dd7167
David Guzman <d.guzman@ucl.ac.uk>
2016-09-28 14:07:40 +0100

    Implemented timeout in Login module [ci skip]

--- eidpng/app/login.component.ts
+++ eidpng/app/login.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   templateUrl: 'app/login.component.html',
   styleUrls: ['app/login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  timeout = 600; // 10 minutes
+  timeout = 180; // 3 minutes
   pgbValue = 1;
   heading = 'EIDP4 Database';
 
   constructor(private _router: Router) {}
 
+  ngOnInit() {
+    console.log('ngOnInit');
+    setInterval(() => this.addIncrement(), 1000);
+  }
+
   onLogin() {
     this._router.navigate(['d']);
   }
 
+  addIncrement() {
+        if (this.pgbValue == this.timeout) {
+            this.redirectToCover();
+        } else {
+            this.pgbValue += 1;
+        }
+    }
+
   resetProgressBar() {
     this.pgbValue = 1;
+  }
+
+  redirectToCover() {
+        this._router.navigate(['/h'])
   }
 }
