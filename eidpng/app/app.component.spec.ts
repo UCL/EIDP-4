6b281bbb74ca973f205f535a42c7e4a63886dc91
David Guzman <d.guzman@ucl.ac.uk>
2016-10-06 17:27:25 +0100

    Async should be used with external templates

--- eidpng/app/app.component.spec.ts
+++ eidpng/app/app.component.spec.ts
@@ -1,30 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
-let comp: AppComponent;
-let fixture: ComponentFixture<AppComponent>;
-
 describe('AppComponent', () => {
-  beforeEach( () => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ]
-    })
-    .compileComponents()
-    .then(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      comp    = fixture.componentInstance;
-    });
-  });
-  tests();
-});
 
-function tests() {
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+      });
+  }));
 
   it('can instantiate it', () => {
     expect(comp).not.toBeNull();
   });
 
-}
+});
